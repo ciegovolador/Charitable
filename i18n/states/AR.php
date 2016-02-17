4bcf9eaf45af72e9b7bbf78475023ec5fe1fa416
ciegovolador <m.a.esquenon@hotmail.com>
2016-02-17 00:25:18 -0300

    Adding Argentina

---
+++ i18n/states/AR.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Argentina states
+ *
+ * Credit to WooThemes, mikejolly and shivapoudel. This is shamelessly based on their implementation in WooCommerce.
+ *
+ * @since		1.0.0
+ * @package		Charitable/i18n
+ * @author 		Mauro A Esquenon from Eric Daams
+ * @copyright 	Copyright (c) 2015, Studio 164a
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License  
+ */
+
+return array(
+	'PBA'  => __( 'Provincia de Buenos Aires', 'charitable' ),
+	'CABA' => __( 'Ciudad Autónoma De Buenos Aires', 'charitable' ),
+	'CHO' => __( 'Chaco', 'charitable' ),
+	'CHT'  => __( 'Chubut', 'charitable' ),
+	'CBA' => __( 'Córdoba', 'charitable' ),
+	'COR'  => __( 'Corrientes', 'charitable' ),
+	'ER' => __( 'Entre Ríos', 'charitable' ),
+	'FSA' => __( 'Formosa', 'charitable' ),
+	'JUJ' => __( 'Jujuy', 'charitable' ),
+	'LP'  => __( 'La Pampa', 'charitable' ),
+	'LR' => __( 'La Rioja', 'charitable' ),
+	'MZA' => __( 'Mendoza', 'charitable' ),
+	'MIS' => __( 'Misiones', 'charitable' ),
+	'NEU'  => __( 'Neuquén', 'charitable' ),
+	'RN' => __( 'Río Negro', 'charitable' ),
+	'SAL' => __( 'Salta', 'charitable' ),
+	'SJ' => __( 'San Juan', 'charitable' ),
+	'SL' => __( 'San Luis', 'charitable' ),
+	'SE' => __( 'Santiago Del Estero', 'charitable' ),
+	'TF' => __( 'Tierra Del Fuego', 'charitable' ),
+	'TUC' => __( 'Tucumán', 'charitable' )
+);
